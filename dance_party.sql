6608e756b8920c6c20b50d67dc092cfca42fc332
lawer <1195288121@qq.com>
2018-04-17 19:38:38 +0800

    4/17

--- dance_party.sql
+++ dance_party.sql
@@ -1,29 +1,33 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: dance_party
--- ------------------------------------------------------
--- Server version	5.7.21-0ubuntu0.16.04.1
+-- Host: localhost
+-- Generation Time: 2018-04-16 22:16:48
+-- 服务器版本： 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `activity`
+-- Database: `dance_party`
 --
 
-DROP TABLE IF EXISTS `activity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `activity`
+--
+
 CREATE TABLE `activity` (
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增编号',
+  `id` int(10) NOT NULL COMMENT '自增编号',
   `name` varchar(100) NOT NULL COMMENT '活动名称',
   `label` varchar(256) NOT NULL COMMENT '活动标签',
   `time` datetime NOT NULL COMMENT '活动时间',
@@ -34,130 +38,167 @@ CREATE TABLE `activity` (
   `join_dancer` text NOT NULL COMMENT '参加人员',
   `place` text NOT NULL COMMENT '活动地址',
   `create_time` datetime NOT NULL COMMENT '创建时间',
-  `note` varchar(256) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`)
+  `note` varchar(256) DEFAULT NULL COMMENT '备注'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='活动表';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `activity`
+-- 转存表中的数据 `activity`
 --
 
-LOCK TABLES `activity` WRITE;
-/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `activity` (`id`, `name`, `label`, `time`, `time_len`, `join_num`, `pub_dancer`, `status`, `join_dancer`, `place`, `create_time`, `note`) VALUES
+(1, 'dance', '地板舞, 震感舞', '2018-04-02 00:00:00', 2, 2, '劳华', '0', '鸡威,劳华', 'a1', '2018-04-01 00:00:00', NULL),
+(2, 'powermove', '1', '2018-03-18 19:00:00', 2, 1, '劳华', '1', '劳华', 'A1', '2018-04-16 15:55:35', '死了都要练');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `label`
+-- 表的结构 `label`
 --
 
-DROP TABLE IF EXISTS `label`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `label` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签id',
-  `name` varchar(256) NOT NULL COMMENT '标签名',
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL COMMENT '标签id',
+  `name` varchar(256) NOT NULL COMMENT '标签名'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签表';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `label`
+-- 转存表中的数据 `label`
 --
 
-LOCK TABLES `label` WRITE;
-/*!40000 ALTER TABLE `label` DISABLE KEYS */;
-/*!40000 ALTER TABLE `label` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `label` (`id`, `name`) VALUES
+(1, 'breaking');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `school`
+-- 表的结构 `school`
 --
 
-DROP TABLE IF EXISTS `school`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `school` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学校id',
+  `id` int(11) NOT NULL COMMENT '学校id',
   `name` varchar(256) NOT NULL COMMENT '学校名称',
-  `city` varchar(256) NOT NULL COMMENT '所在城市',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='学校表';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `city` varchar(256) NOT NULL COMMENT '所在城市'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学校表';
 
 --
--- Dumping data for table `school`
+-- 转存表中的数据 `school`
 --
 
-LOCK TABLES `school` WRITE;
-/*!40000 ALTER TABLE `school` DISABLE KEYS */;
-INSERT INTO `school` VALUES (1,'华南理工大学','广州'),(3,'中山大学','广州'),(4,'广东工业大学','广州');
-/*!40000 ALTER TABLE `school` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `school` (`id`, `name`, `city`) VALUES
+(1, '华南理工大学', '广州'),
+(3, '中山大学', '广州'),
+(4, '广东工业大学', '广州');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- 表的结构 `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `id` int(11) NOT NULL COMMENT '用户id',
   `name` varchar(256) NOT NULL COMMENT '用户名称',
   `school` varchar(256) DEFAULT NULL,
   `label` varchar(256) NOT NULL COMMENT '标签',
   `gender` varchar(256) NOT NULL COMMENT '性别',
   `phone` text NOT NULL COMMENT '电话',
   `type` varchar(256) NOT NULL COMMENT '身份类型',
-  `img_url` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `img_url` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- 转存表中的数据 `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user` (`id`, `name`, `school`, `label`, `gender`, `phone`, `type`, `img_url`) VALUES
+(1, '劳华', '华南理工大学', '地板舞', '男', '18925830351', '学生', NULL),
+(2, 'jv', '华南理工大学', '锁舞', '男', '123456789', '学生', NULL);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `user_activity`
+-- 表的结构 `user_activity`
 --
 
-DROP TABLE IF EXISTS `user_activity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_activity` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增编号',
+  `id` int(11) NOT NULL COMMENT '自增编号',
   `user_id` int(11) NOT NULL COMMENT '用户id',
   `activity_id` int(11) NOT NULL COMMENT '活动id',
   `pub_status` int(11) NOT NULL COMMENT '发布状态',
-  `join_status` int(11) NOT NULL COMMENT '参与状态',
-  PRIMARY KEY (`id`)
+  `join_status` int(11) NOT NULL COMMENT '参与状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户活动表';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_activity`
+-- 转存表中的数据 `user_activity`
 --
 
-LOCK TABLES `user_activity` WRITE;
-/*!40000 ALTER TABLE `user_activity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_activity` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `user_activity` (`id`, `user_id`, `activity_id`, `pub_status`, `join_status`) VALUES
+(1, 1, 0, 1, 1),
+(2, 1, 1, 1, 1),
+(3, 1, 1, 1, 1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `label`
+--
+ALTER TABLE `label`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_activity`
+--
+ALTER TABLE `user_activity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增编号', AUTO_INCREMENT=3;
+--
+-- 使用表AUTO_INCREMENT `label`
+--
+ALTER TABLE `label`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签id', AUTO_INCREMENT=2;
+--
+-- 使用表AUTO_INCREMENT `school`
+--
+ALTER TABLE `school`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学校id', AUTO_INCREMENT=5;
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id', AUTO_INCREMENT=3;
+--
+-- 使用表AUTO_INCREMENT `user_activity`
+--
+ALTER TABLE `user_activity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增编号', AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-03-29  0:02:18
--- js/my_info.js
+++ js/my_info.js
@@ -101,3 +101,72 @@ $(document).ready(function () {
 		})
 	})
 })
+
+function upLoad() {
+	var file = $('.photo').find('input')[0].files[0];
+	//console.log(file);
+
+	var reader = new FileReader();
+	reader.onload = function(e){
+		var imgFile = e.target.result;
+		console.log(imgFile);
+		$('.photo-img').attr('src',imgFile);
+		$('.photo-img').attr('style','display:block');
+	}
+	reader.readAsDataURL(file);
+}
+
+function nextStep() {
+
+	var name = $('input[name = name]').val();
+	var identity = $('.chosen-identity').text();
+	var school = $('select[name = school] .right').text();
+	var gender = $('.chosen-gender').text();
+	var phone = $('input[name = phone]').val();
+	var label = '';
+	$.each($('.active'), function () {
+		if (label) {
+			label += ',' + $(this).text();
+		}
+		else {
+			label += $(this).text();
+		}
+	});
+
+	//头像图片
+	var img_url = $('.photo-img').attr('src');
+
+	//社会工作者无须选择学校，是学生但未选择学校要重新输入
+	if(!name || !gender || !phone || !label || (identity =="学生" && school=="选择你的大学")){
+		alert("未全部填写完成!");
+	}
+	else{
+		$.ajax({
+		    url: domain + '/update/user/info',
+		    type: 'post',
+		    dataType: 'json',
+		    data: {
+				name:name,
+				school: school,
+				label: label,
+				gender: gender,
+				phone: phone,
+				img_url: img_url	    	
+		    },
+		    success: function(data) {
+		        if(data.code == 0){
+		        	//console.log(data.data.id);
+		        	alert("修改成功！");
+		        	setStorage('user_id',data.data.id);
+					window.location.href = 'mine.html';
+		        }
+        		else {
+		          alert("修改失败！");
+		        }
+		    },
+		    error: function(err) { 
+		      console.log(err);
+		    }
+		});
+	}
+}
