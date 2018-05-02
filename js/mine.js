741de7c7ac599a9317a340193b5573fbe947878d
sdMac <553828127@qq.com>
2018-05-02 10:55:50 +0800

    href or click

--- js/mine.js
+++ js/mine.js
@@ -5,30 +5,30 @@ var userId = getStorage('userId');
 $(document).ready(function () {
 
 	// 判断是否已经注册
-	if (userId) {
-		$('.unregister').hide();
-		$('header a').attr('href', 'my_info.html');
-		$.ajax({
-    	url: domain + '/user/info',
-			type: 'get',
-			data : {
-				user_id: userId
-			},
-			datatype : 'json',
-			success: function(data) {
-				data = JSON.parse(data);
-				//用户已注册
-				$('.pass').attr('src', data.data.img_url);
-				$('.name').text(data.data.name);
-				$('.role').text(data.data.type);
-				$('.school').text(data.data.school);
-		   }
-		});
-	} else {
-		$('header a').attr('href', 'register.html');
-		// 显示注册按钮
-		$('.name').hide();
-		$('.role').hide();
-		$('.school').hide();
-	}
+	// if (userId) {
+	// 	$('.unregister').hide();
+	// 	$('header a').attr('href', 'my_info.html');
+	// 	$.ajax({
+ //    	url: domain + '/user/info',
+	// 		type: 'get',
+	// 		data : {
+	// 			user_id: userId
+	// 		},
+	// 		datatype : 'json',
+	// 		success: function(data) {
+	// 			data = JSON.parse(data);
+	// 			//用户已注册
+	// 			$('.pass').attr('src', data.data.img_url);
+	// 			$('.name').text(data.data.name);
+	// 			$('.role').text(data.data.type);
+	// 			$('.school').text(data.data.school);
+	// 	   }
+	// 	});
+	// } else {
+	// 	$('header a').attr('href', 'register.html');
+	// 	// 显示注册按钮
+	// 	$('.name').hide();
+	// 	$('.role').hide();
+	// 	$('.school').hide();
+	// }
 })
